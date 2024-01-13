0a724de60e82148c863e4cb3816136660fc7c323
Micah Coldiron <mcc4339@rit.edu>
2024-01-13 14:21:04 -0800

    Javascript and html should be all fixed up now, and I also added the css code.

--- Homework 1/homework1.js
+++ Homework 1/homework1.js
@@ -1,18 +1,14 @@
 let words = new string[3];
 for (i = 0; i < length(words) - 1; i++) {
     words[i] = prompt("Please enter a word, any word.", "Type your response here.");
-    console.log(words[i]);
+    document.getElementById("myTextBox" + i).log(words[i]);
 }
-document.getElementById("placeholder").style.visibility=systemON?"‌​visible":"visible";
-document.getElementById("placeholder").addEventListener("onclick", function () {
+document.getElementById("submitb").style.visibility=systemON?"‌​visible":"visible";
+document.getElementById("submitb").addEventListener("onclick", function () {
     let words2 = words.map(function (word) { 
         return word[word.length - 1] + word.substring(1, (word.length - 2)) + word[0];
     })
-    console.log(words2.toString());
+    document.getElementById("myTextBox4").log(words2[0]);
+    document.getElementById("myTextBox5").log(words2[1]);
+    document.getElementById("myTextBox6").log(words2[2]);
 })
-document.addEventListener("DOMContentLoaded", function (event) {
-
-});
-   function transformWords() {
-        // Your code here
-    }
