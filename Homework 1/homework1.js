afdc8a3528a8559247f82494f7552fde9990c90d
Micah Coldiron <mcc4339@rit.edu>
2024-01-12 16:33:47 -0800

    fixed a bug

--- Homework 1/homework1.js
+++ Homework 1/homework1.js
@@ -4,7 +4,7 @@ for (i = 0; i < length(words) - 1; i++) {
     console.log(words[i]);
 }
 document.getElementById("placeholder").style.visibility=systemON?"‌​visible":"visible";
-document.getElementById("placeholder").addEventListener("onclick", function {
+document.getElementById("placeholder").addEventListener("onclick", function () {
     let words2 = words.map(function (word) { 
         return word[word.length - 1] + word.substring(1, (word.length - 2)) + word[0];
     })
