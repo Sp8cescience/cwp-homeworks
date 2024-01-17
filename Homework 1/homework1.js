75d6aa6c2b086b63f256a6cf1697b976ffb7748f
Micah Coldiron <mcc4339@rit.edu>
2024-01-16 19:47:22 -0800

    more bug fixes

--- Homework 1/homework1.js
+++ Homework 1/homework1.js
@@ -1,7 +1,7 @@
 let words = [3];
 for (i = 0; i < words.length - 1; i++) {
     words[i] = prompt("Please enter a word, any word.", "Type your response here.");
-    document.getElementById("myTextBox" + i).write(words[i]);
+    document.getElementById("myTextBox" + i).value = words[i];
 }
 document.getElementById("submitb").style.visibility="visible";
 document.getElementById("submitb").addEventListener("onclick", function () {
@@ -10,7 +10,7 @@ document.getElementById("submitb").addEventListener("onclick", function () {
     let words2 = words.map(function (word) { 
         return word[word.length - 1] + word.substring(1, (word.length - 2)) + word[0];
     })
-    document.getElementById("myTextBox4").write(words2[0]);
-    document.getElementById("myTextBox5").write(words2[1]);
-    document.getElementById("myTextBox6").write(words2[2]);
+    document.getElementById("myTextBox4").value = words2[0];
+    document.getElementById("myTextBox5").value = words2[1];
+    document.getElementById("myTextBox6").value = words2[2];
 })
