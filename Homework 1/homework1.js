55a615e69970562f5cc1dbf09f928a86326d5448
Micah Coldiron <mcc4339@rit.edu>
2024-01-16 18:45:53 -0800

    made the textboxes appear and disappear as per the instructions.

--- Homework 1/homework1.js
+++ Homework 1/homework1.js
@@ -3,8 +3,10 @@ for (i = 0; i < length(words) - 1; i++) {
     words[i] = prompt("Please enter a word, any word.", "Type your response here.");
     document.getElementById("myTextBox" + i).log(words[i]);
 }
-document.getElementById("submitb").style.visibility=systemON?"‌​visible":"visible";
+document.getElementById("submitb").style.visibility="visible";
 document.getElementById("submitb").addEventListener("onclick", function () {
+    document.getElementById("textbox1").style.visbility="hidden";
+    document.getElementById("textbox2").style.visibilty="visible";
     let words2 = words.map(function (word) { 
         return word[word.length - 1] + word.substring(1, (word.length - 2)) + word[0];
     })
